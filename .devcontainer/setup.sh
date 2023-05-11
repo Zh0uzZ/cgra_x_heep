10539dbf07f1ed9e257dc94511491870ec95e963
hank <zhouzzmy@163.com>
2023-05-11 16:19:37 +0800

    add .devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,14 @@
+# user alias
+alias la="ls -al"
+
+# conda
+source /opt/conda/etc/profile.d/conda.sh
+
+# oss-cad-suite
+export PATH=$PATH:/opt/oss-cad-suite/bin
+
+# riscv-gnu-toolchain
+export PATH=$PATH:/opt/riscv32/bin
+
+# verible
+export PATH=$PATH:/opt/verible/bin
